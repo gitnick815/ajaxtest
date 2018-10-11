4979d91ee72f25a89e91f7fc520cf2b7cf18cb07
王鼎恩 <nick9815@gmail.com>
2018-10-11 22:54:30 +0800

    one

---
+++ AJAX.js
@@ -0,0 +1,44 @@
+//AJAX
+
+function loadXMLDoc() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("demo").innerHTML = xhttp.responseText;
+        }
+        console.log(xhttp.readyState + " " + xhttp.status);
+    }
+    var url = "https://drive.google.com/file/d/1l9JY4cEWFCyD3CthZBbvyieyvO2pDlup/view?usp=sharing"
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    // document.getElementById("demo").innerHTML = xhttp.responseText;
+}
+
+function showHint(str)
+{
+var xmlhttp;
+if (str.length==0)
+  { 
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","gethint.asp?q="+str,true);
+xmlhttp.send();
+
+}
